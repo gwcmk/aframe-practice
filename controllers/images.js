1f1a15483f95642f79cf008569db1a1f68e7e91c
gwcmk <gregwcmkim@gmail.com>
2017-01-10 10:20:19 -0600

    Started work on URL validation

--- controllers/images.js
+++ controllers/images.js
@@ -1,11 +1,22 @@
 'use strict';
 const views = require('co-views');
 const parse = require('co-body');
+const url = require('url');
 
 const render = views(__dirname + '/../views', {
   map: { html: 'swig' }
 });
 
-module.exports.aframe = function *aframe(image) {
-  this.body = yield render('aframe', { 'path': `../img/${image}` });
+module.exports.aframe = function *aframe(path) {
+  console.log(path);
+  let src = isURL(path) ? path : `../img/${path}`;
+  console.log(src);
+  this.body = yield render('aframe', { 'src': src });
 };
+
+// source: http://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url/22648406#22648406
+function isURL(str) {
+  var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+  var url = new RegExp(urlRegex, 'i');
+  return str.length < 2083 && url.test(str);
+}
