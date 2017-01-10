d446f082dc756c31aa760678d0645ed41d54d148
gwcmk <gregwcmkim@gmail.com>
2017-01-10 09:21:53 -0600

    Initial commit

---
+++ controllers/images.js
@@ -0,0 +1,11 @@
+'use strict';
+const views = require('co-views');
+const parse = require('co-body');
+
+const render = views(__dirname + '/../views', {
+  map: { html: 'swig' }
+});
+
+module.exports.aframe = function *aframe(image) {
+  this.body = yield render('aframe', { 'path': `../img/${image}` });
+};
